48527e34e79cbf9554efc50088e010d740dd7082
Jason Pinlac <jasonpinlacdev@gmail.com>
2025-10-31 12:21:24 -0400

    set up initial project

---
+++ Auth0AuthenticationPOC/Auth0AuthenticationPOC/Controllers/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  Auth0AuthenticationPOC
+//
+//  Created by Jason Pinlac on 10/31/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.systemYellow
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
